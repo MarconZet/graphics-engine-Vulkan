8a0317c8c93e80ce3d6c153935bca9c2c45dd7aa
MarconZet <marcin.zlakowski@gmail.com>
2018-09-09 02:40:41 +0200

    Instance

--- src/main/java/pl/marconzet/engine/HelloTriangleApplication.java
+++ src/main/java/pl/marconzet/engine/HelloTriangleApplication.java
@@ -1,6 +1,20 @@
 package pl.marconzet.engine;
 
+import org.lwjgl.BufferUtils;
+import org.lwjgl.PointerBuffer;
+import org.lwjgl.vulkan.VkApplicationInfo;
+import org.lwjgl.vulkan.VkExtensionProperties;
+import org.lwjgl.vulkan.VkInstance;
+import org.lwjgl.vulkan.VkInstanceCreateInfo;
+
+import java.nio.IntBuffer;
+
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.glfw.GLFWVulkan.glfwGetRequiredInstanceExtensions;
+import static org.lwjgl.system.MemoryUtil.memUTF8;
+import static org.lwjgl.vulkan.VK10.*;
+import static pl.marconzet.engine.VKUtil.translateVulkanResult;
+
 
 /**
  * @author MarconZet
@@ -10,8 +24,9 @@ public class HelloTriangleApplication {
     private static final int WIDTH = 800;
     private static final int HEIGHT = 600;
 
-    private long window;
 
+    private long window;
+    private VkInstance instance;
 
     public void run(){
         initWindow();
@@ -30,7 +45,39 @@ public class HelloTriangleApplication {
     }
 
     private void initVulkan() {
+        VkApplicationInfo appInfo = VkApplicationInfo.calloc()
+                .sType(VK_STRUCTURE_TYPE_APPLICATION_INFO)
+                .pApplicationName(memUTF8("Hello Triangle"))
+                .applicationVersion(VK_MAKE_VERSION(1, 0, 0))
+                .pEngineName(memUTF8("No Engine"))
+                .engineVersion(VK_MAKE_VERSION(1, 0, 0))
+                .apiVersion(VK_API_VERSION_1_0);
 
+        PointerBuffer pointerBuffer = glfwGetRequiredInstanceExtensions();
+        VkInstanceCreateInfo pCreateInfo = VkInstanceCreateInfo.calloc()
+                .sType(VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO)
+                .pApplicationInfo(appInfo)
+                .ppEnabledExtensionNames(pointerBuffer);
+
+        IntBuffer pExtensionCount = BufferUtils.createIntBuffer(1);
+        vkEnumerateInstanceExtensionProperties((CharSequence) null, pExtensionCount, null);
+        int extensionCount = pExtensionCount.get();
+        VkExtensionProperties.Buffer extensions = new VkExtensionProperties.Buffer(BufferUtils.createByteBuffer(VkExtensionProperties.SIZEOF * extensionCount));
+        pExtensionCount.clear();
+        vkEnumerateInstanceExtensionProperties((CharSequence) null, pExtensionCount, extensions);
+        System.out.println("Available extensions:");
+        while(extensions.hasRemaining()){
+            System.out.println(extensions.get().extensionNameString());
+        }
+
+        PointerBuffer pInstance = BufferUtils.createPointerBuffer(1);
+        int err = vkCreateInstance(pCreateInfo, null, pInstance);
+        long instance = pInstance.get();
+        if (err != VK_SUCCESS) {
+            throw new AssertionError("Failed to create VkInstance: " + translateVulkanResult(err));
+        }
+
+        this.instance = new VkInstance(instance, pCreateInfo);
     }
 
     private void mainLoop() {
@@ -40,6 +87,8 @@ public class HelloTriangleApplication {
     }
 
     private void cleanup() {
+        vkDestroyInstance(instance, null);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
